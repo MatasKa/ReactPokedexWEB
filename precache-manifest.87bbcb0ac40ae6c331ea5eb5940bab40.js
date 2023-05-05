322b7b17136d37fa548df2d6c31d256f0504475a
Matas K <matas.k2002@gmail.com>
2023-05-05 12:31:04 +0200

    Updates

---
+++ precache-manifest.87bbcb0ac40ae6c331ea5eb5940bab40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94e277b828b4317095b09831755f658",
+    "url": "/react-gh-pages/ReactPokedexWEB/index.html"
+  },
+  {
+    "revision": "e3b8d13d4e10d47e4eec",
+    "url": "/react-gh-pages/ReactPokedexWEB/static/css/main.92c4fb02.chunk.css"
+  },
+  {
+    "revision": "ed7512cdf3e174b4300e",
+    "url": "/react-gh-pages/ReactPokedexWEB/static/js/2.3d75e165.chunk.js"
+  },
+  {
+    "revision": "e4f4d60ab36518f118e17022748fc78f",
+    "url": "/react-gh-pages/ReactPokedexWEB/static/js/2.3d75e165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b8d13d4e10d47e4eec",
+    "url": "/react-gh-pages/ReactPokedexWEB/static/js/main.09b2315f.chunk.js"
+  },
+  {
+    "revision": "7d2ded91d88747f7688a",
+    "url": "/react-gh-pages/ReactPokedexWEB/static/js/runtime-main.92fccee9.js"
+  },
+  {
+    "revision": "3fa722008913cd41a31de9388d169b52",
+    "url": "/react-gh-pages/ReactPokedexWEB/static/media/PokeAPI.3fa72200.png"
+  }
+]);
